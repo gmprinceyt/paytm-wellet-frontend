4946f504f987c4f160a36bdbc1a13cbaf3b1e05f
PRINCE KUAMR <gmprince420@gmail.com>
2025-08-26 21:52:42 +0530

    initail

---
+++ src/pages/Singup.jsx
@@ -0,0 +1,18 @@
+const SingUp = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+        backgroundColor: "gray"
+      }}
+    >
+        
+    </div>
+  );
+};
+
+export default SingUp;
